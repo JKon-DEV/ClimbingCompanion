188f8b9e5927442af21d53e9e5dcc8b5770d4106
JKON-DEV <jared8454@gmail.com>
2026-01-11 16:15:13 +1100

    Added a main menu on launch to accommodate future addition of features.
    next to add is a timer system for climbing finger training. general brainstorm of the system complete but need to implement.
    
    - should have full CRUD for timing layouts

---
+++ app/src/main/java/com/example/bouldercompanion/ui/MainMenuActivity.kt
@@ -0,0 +1,30 @@
+package com.example.bouldercompanion.ui
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bouldercompanion.R
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+
+        val viewClimbListBtn = findViewById<Button>(R.id.climbListButton)
+        val trainingBtn = findViewById<Button>(R.id.trainingButton)
+
+        viewClimbListBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        trainingBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
